8ffa8a64318f9103876e084a9c80b13a03cbad6d
Alan Brandi <alan.brandi@hotmail.com>
2021-09-08 22:58:08 -0300

    Adicionar arquivos de projeto.

---
+++ Estoque/Produtos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Produtos
+    {
+        //Declarando variáveis.
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        //Adicionando um construtor padrão.
+        public Produtos (string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        //Calculo do valor total em estoque.
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        //Adicionando a quandidade de um produto junto da quantidade enviada.
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        //Retirando uma quantidade do produto.
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+        //Atualizando os dados junto de refinamentos das desmonstração para o usuário.
+        public override string ToString()
+        {
+            return Nome + ", R$" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Estoque/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //Descrevendo e pedindo os dados para o usuário + armazenando os dados do usuário.
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.WriteLine("Nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.CurrentCulture);
+            Console.WriteLine("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            //Declarando e instanciando objeto Produtos.
+            Produtos p = new Produtos(nome, preco, quantidade);
+
+            //Demonstrando para o usuário seus resultados já somados.
+            Console.WriteLine("Dados do produto: " + p);
+
+            //Descrevendo para o usuário adicionar novas quantias do mesmo produto.
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            //Declarando a quantidade de novos produtos.
+            int qte = int.Parse(Console.ReadLine());
+            //Adicinando o produto a quantidade que ja possuí no estoque.
+            p.AdicionarProduto(qte);
+            //Atualizando os dados.
+            Console.WriteLine("Dados atualizados: " + p);
+            //Retirando a quantidade desejada pelo usuário.
+            Console.WriteLine("Digite um número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+            //Atualizando os dados.
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
